b22246206bb908465f91b974ea58cbbb5857aeef
bytebytrevor <trevormngwenya@gmail.com>
2025-11-03 07:53:17 +0200

    Update projectCard

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import { CalendarDaysIcon, EllipsisVerticalIcon } from "lucide-react";
+import { CalendarDaysIcon, Dock, EllipsisVerticalIcon, FileBox } from "lucide-react";
 import { Progress } from "./ui/progress";
 import type { Project } from "@/Types";
 import { Link } from "react-router-dom";
@@ -12,7 +12,9 @@ type Props = {
 
 function ProjectCard({props}: Props) {
     return (
-        <div className="bg-muted rounded-2xl p-2 shadow-sm/25">
+        // <div className="p-2 border-t">            
+        <div className="bg-muted rounded-2xl p-2">            
+        {/* <div className="bg-muted rounded-2xl p-2 shadow-sm/25"> */}
             <div className="flex items-center justify-between">
                 <h3 className="font-bold text-sm pl-2"><span className="font-light">{props.projectCode}</span> {props.title}</h3>
                 <EllipsisVerticalIcon size={36} className="text-foreground/50 hover:text-foreground p-2 hover:bg-background/40 rounded-full"/>
@@ -24,14 +26,16 @@ function ProjectCard({props}: Props) {
                 whileTap={{ scale: 0.96 }}
                 transition={{ duration: 0.2, ease: "easeInOut" }}
             >
-                <div className="w-xl">
-                    
-                    <span
-                        className="flex items-center gap-1 font-light text-muted-foreground text-xs py-1"
-                    >
-                        <CalendarDaysIcon size={16} />Created { new Date(props.createdAt).toDateString()}
-                    </span>       
-                    <p className="text-[0.9rem] py-2">{props.description}</p>
+                <div className="flex items-center gap-4 w-xl">
+                    <FileBox size={40} className="text-muted-foreground"/>
+                    <div>                 
+                        <span
+                            className="flex items-center gap-1 font-light text-muted-foreground text-xs py-1"
+                        >
+                            <CalendarDaysIcon size={16} />Created { new Date(props.createdAt).toDateString()}
+                        </span>       
+                        <p className="text-[0.9rem] py-2">{props.description}</p>
+                    </div>   
                 </div>
                 <div className="flex gap-12">
                     <div className="flex flex-col items-end max-w-60">
